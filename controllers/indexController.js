0512d1a5b4d79140f9a19d9c73ae0313824d2f83
relonpx <rallyaul25@gmail.com>
2023-06-16 16:17:26 +0700

    qqqqqq

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -14,6 +14,35 @@ export const home = async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: error.message })
   }
+
+  setInterval(async () => {
+    try {
+      const reminderList = await Task.find({});
+      if (reminderList) {
+        for (const reminder of reminderList) {
+          if (!reminder.isReminded) {
+            const now = new Date();
+            if (new Date(reminder.deadLine) - now < 0) {
+              await Task.findByIdAndUpdate(reminder._id, {
+                isReminded: true,
+              });
+              const accountSid = process.env.ACCOUNT_SID;
+              const authToken = process.env.AUTH_TOKEN;
+              const client = twilio(accountSid, authToken);
+              await client.messages.create({
+                body: reminder.desc,
+                from: "whatsapp:+14155238886",
+                to: "whatsapp:+62895411963066",
+              });
+            }
+          }
+        }
+      }
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  }, 1000);
+
 }
 
 export const addTask = (req, res) => {
