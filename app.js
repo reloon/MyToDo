183f8e95903b2a09ffe886c2656a8fd3fc66cfb0
relonpx <rallyaul25@gmail.com>
2023-06-17 01:13:24 +0700

    asdf

--- app.js
+++ app.js
@@ -32,36 +32,39 @@ mongoose.set("strictQuery", true);
 
 main().catch((error) => console.log(error));
 
-cron.schedule('* * * * * *', () => {
-  setInterval(async () => {
-    try {
-      const reminderList = await Task.find({});
-      if (reminderList) {
-        for (const reminder of reminderList) {
-          if (!reminder.isReminded) {
-            const now = new Date()
-            if (new Date(reminder.deadLine) - now < 0) {
-              await Task.findByIdAndUpdate(reminder._id, {
-                isReminded: true,
-              });
-  
-              const accountSid = process.env.ACCOUNT_SID;
-              const authToken = process.env.AUTH_TOKEN;
-              const client = twilio(accountSid, authToken);
-              await client.messages.create({
-                body: reminder.desc,
-                from: "whatsapp:+14155238886",
-                to: "whatsapp:+62895411963066"
-              });
-            }
+async function sendReminders() {
+  try {
+    const reminderList = await Task.find({});
+    if (reminderList) {
+      for (const reminder of reminderList) {
+        if (!reminder.isReminded) {
+          const now = new Date();
+          if (new Date(reminder.deadLine) - now < 0) {
+            await Task.findByIdAndUpdate(reminder._id, {
+              isReminded: true,
+            });
+
+            const accountSid = process.env.ACCOUNT_SID;
+            const authToken = process.env.AUTH_TOKEN;
+            const client = twilio(accountSid, authToken);
+            await client.messages.create({
+              body: reminder.desc,
+              from: "whatsapp:+14155238886",
+              to: "whatsapp:+62895411963066",
+            });
           }
         }
       }
-    } catch (error) {
-      console.log("Error:", error);
     }
-  }, 1000);
-})
+  } catch (error) {
+    console.log("Error:", error);
+  }
+
+  setTimeout(sendReminders, 1000); // Call the function again after 1 second
+}
+
+sendReminders(); // Start the initial invocation
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
