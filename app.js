d279ee94ea7bdd3770f646833ba8a1e044ac5f36
relonpx <rallyaul25@gmail.com>
2023-06-17 03:14:31 +0700

    lol

--- app.js
+++ app.js
@@ -5,10 +5,7 @@ import bodyParser from 'body-parser';
 import expressLayouts from "express-ejs-layouts";
 import crud from './routes/crudRoute.js'
 import indexRouter from './routes/indexRoute.js'
-import twilio from 'twilio'
-import Task from './models/taskModel.js'
 import dotenv from 'dotenv'
-import cron from 'node-cron'
 dotenv.config()
 
 const app = express();
@@ -31,35 +28,6 @@ const main = async (req, res) => {
 mongoose.set("strictQuery", true);
 
 main().catch((error) => console.log(error));
-
-cron.schedule('* * * * *', async () => {
-  try {
-    const reminderList = await Task.find({});
-    if (reminderList) {
-      for (const reminder of reminderList) {
-        if (!reminder.isReminded) {
-          const now = new Date();
-          if (new Date(reminder.deadLine) - now < 0) {
-            await Task.findByIdAndUpdate(reminder._id, {
-              isReminded: true,
-            });
-
-            const accountSid = process.env.ACCOUNT_SID;
-            const authToken = process.env.AUTH_TOKEN;
-            const client = twilio(accountSid, authToken);
-            await client.messages.create({
-              body: reminder.desc,
-              from: "whatsapp:+14155238886",
-              to: "whatsapp:+62895411963066",
-            });
-          }
-        }
-      }
-    }
-  } catch (error) {
-    console.log("Error:", error);
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
