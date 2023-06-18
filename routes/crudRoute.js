d238f59c7f7592663a421f25bd8e65cd139fd06b
relonpx <rallyaul25@gmail.com>
2023-06-18 20:03:43 +0700

    nothing

--- routes/crudRoute.js
+++ routes/crudRoute.js
@@ -1,22 +1,48 @@
 import express from "express";
 import Task from '../models/taskModel.js'
+import twilio from 'twilio'
+import dotenv from "dotenv";
+dotenv.config();
 
 const router = express.Router()
+const accountSid = process.env.ACCOUNT_SID
+const authToken = process.env.AUTH_TOKEN
+const client = twilio(accountSid, authToken)
 
-router.post('/add', (req, res) => {
-  const body = req.body
+router.post("/add", (req, res) => {
+  const { task, desc, deadline } = req.body;
 
-  const task = new Task({
-    task: body.task,
-    desc: body.desc,
-    deadLine: body.deadline,
-    isReminded: false
-  })
-  task.save().then(() => {
-    console.log(req.body)
-    res.redirect('/')
-  })
-})
+  const taskData = {
+    task: task,
+    desc: desc,
+    deadLine: deadline,
+  };
+
+  const tasks = new Task(taskData);
+
+  tasks
+    .save()
+    .then(() => {
+      console.log("Task added:", taskData);
+      res.redirect("/");
+    })
+    .catch((error) => {
+      console.log("Error saving task:", error);
+      res.status(500).send("Error saving task.");
+    });
+
+  const twilioMessage = `Hello Reloon, there is a new ${task} assignment => *${desc}*.
+Must be collected before ${deadline}`;
+  client.messages
+    .create({
+      body: twilioMessage,
+      from: "whatsapp:+14155238886",
+      to: "whatsapp:+62895411963066",
+    })
+    .then((message) => console.log("Twilio message sent:", message.sid))
+    .catch((error) => console.log("Error sending Twilio message:", error));
+});
+
 
 router.get("/delete/:id", (req, res) => {
   const id = req.params.id;
