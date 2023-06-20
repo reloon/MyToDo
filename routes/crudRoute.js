d3ee62b422d14d745b4957684049f0ed38763b1f
relonpx <rallyaul25@gmail.com>
2023-06-20 23:52:27 +0700

    ampun

--- routes/crudRoute.js
+++ routes/crudRoute.js
@@ -1,15 +1,14 @@
 import express from "express";
-import Task from "../models/taskModel.js";
-import twilio from "twilio";
-import Pusher from "pusher";
+import Task from '../models/taskModel.js'
+import twilio from 'twilio'
 import { Configuration, OpenAIApi } from "openai";
 import dotenv from "dotenv";
 dotenv.config();
 
-const router = express.Router();
+const router = express.Router()
 const accountSid = process.env.ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN;
-const client = twilio(accountSid, authToken);
+const client = twilio(accountSid, authToken)
 
 router.post("/add", (req, res) => {
   const { task, desc, deadline } = req.body;
@@ -45,6 +44,7 @@ Must be collected before ${deadline}`;
     .catch((error) => console.log("Error sending Twilio message:", error));
 });
 
+
 router.get("/delete/:id", (req, res) => {
   const id = req.params.id;
   Task.deleteOne({ _id: id })
@@ -56,34 +56,31 @@ router.get("/delete/:id", (req, res) => {
     });
 });
 
-router.post("/message", async (req, res) => {
-  const message = req.body.message;
-  const pusher = new Pusher({
-    appId: "1621933",
-    key: "f64eba4bedbfc2d41f25",
-    secret: "345149ccfad76e549cd1",
-    cluster: "ap1",
-    useTLS: true,
+router.post('/send', async (req, res) => {
+  const configuration = new Configuration({
+    apiKey: process.env.API_KEY,
   });
-  const configuration = new Configuration({ apiKey: process.env.API_KEY });
   const openai = new OpenAIApi(configuration);
+  const ask = req.body.ask
 
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: message,
+      prompt: ask,
       temperature: 0,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
       max_tokens: 256,
+      stop: ["Human:", "AI:", "Human:", "AI:"],
     });
 
-    const assistantMessage = completion.data.choices[0].text;
-    const response = { text: assistantMessage };
-
-    pusher.trigger("chatBot", "message", response);
-    res.send(message);
+    res.render("chatBot", {
+      title: "Chat Bot",
+      layout: "layouts/main-layout",
+      data: completion.data.choices[0].text,
+      mess: ask
+    });    
   } catch (error) {
     if (error.response) {
       console.error(error.response.status, error.response.data);
@@ -91,6 +88,7 @@ router.post("/message", async (req, res) => {
       console.error(`Error with OpenAI API request: ${error.message}`);
     }
   }
-});
+})
 
-export default router;
+
+export default router
